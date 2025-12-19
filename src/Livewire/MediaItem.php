c1ec7e6b81b8465ff4d6595e932f0f30cd44f76c
Lucas GIRARDOT <lucas.girardot@axn.fr>
2025-12-19 10:11:00 +0100

    remove computed media() method

--- src/Livewire/MediaItem.php
+++ src/Livewire/MediaItem.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Axn\LivewireUploadHandler\Livewire;
 
 use Axn\LivewireUploadHandler\Livewire\Concerns\MediaCommon;
-use Livewire\Attributes\Computed;
 use Livewire\Attributes\Isolate;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -27,7 +26,7 @@ class MediaItem extends Item
             mediaId: $this->itemData['id'],
         );
 
-        $this->media->delete();
+        $this->retrieveMedia($this->itemData['id'])->delete();
 
         $this->itemData['id'] = null;
         $this->savedFileDisk = null;
@@ -39,8 +38,9 @@ class MediaItem extends Item
         $customProperties = $this->mediaFilters;
 
         if ($this->hasSavedFile()) {
-            $customProperties = $this->media->custom_properties;
-            $this->media->delete();
+            $media = $this->retrieveMedia($this->itemData['id']);
+            $customProperties = $media->custom_properties;
+            $media->delete();
         }
 
         $media = $this->model
@@ -66,15 +66,5 @@ class MediaItem extends Item
     protected function mediaSaved(Media $media): void
     {
         //
-    }
-
-    #[Computed]
-    protected function media(): ?Media
-    {
-        if (! isset($this->itemData['id'])) {
-            return null;
-        }
-
-        return $this->retrieveMedia($this->itemData['id']);
     }
 }
